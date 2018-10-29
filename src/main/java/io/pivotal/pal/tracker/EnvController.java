9c8ecd525b60c56b3fac4691060540ce35d7d223
Awesome Man <thomas.j.cramer@accenture.com>
2018-10-29 15:47:08 -0400

    Adding manifest

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,45 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final String address;
+    private final String index;
+    private final String memoryLimit;
+    private String port;
+
+
+//    assertThat(env.get("PORT")).isEqualTo("8675");
+//    assertThat(env.get("")).isEqualTo("12G");
+//    assertThat(env.get("")).isEqualTo("34");
+//    assertThat(env.get("")).isEqualTo("123.sesame.street");
+
+    public EnvController(@Value("${PORT:NOT SET}") String port,
+                         @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:NOT SET}") String index,
+                         @Value("${CF_INSTANCE_ADDR:NOT SET}") String address) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.index = index;
+        this.address = address;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+
+        Map<String, String> env = new HashMap<>();
+        env.put("MEMORY_LIMIT", this.memoryLimit);
+        env.put("PORT", this.port);
+        env.put("CF_INSTANCE_INDEX", this.index);
+        env.put("CF_INSTANCE_ADDR", this.address);
+        return env;
+
+    }
+}
